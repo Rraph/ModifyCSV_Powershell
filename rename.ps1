f0f8beb2c8f7b9655f93699da50e1cf7aa8e6b0c
Raphael BEGHIN <raphael.beghin@gifi.fr>
2019-06-05 14:28:45 +0200

    v1.3

--- rename.ps1
+++ rename.ps1
@@ -1,13 +1,14 @@
 # ================================================================== #
-#                          AUTHOR : RAPHAEL BEGHIN                   #
-#                          NAME : rename.ps1                         #
-#                          DATE : 05/06/2019                         #
-#  FONCTION : remet en forme les prénom et nom des users dans un csv #
+#                       AUTHOR : RAPHAEL BEGHIN                      #
+#                         NAME : rename.ps1                          #
+#                         DATE : 05/06/2019                          #
+#                          VERSION : v 1.3                           #
+#          FUNCTION : change users format in some csv file           #
 # ================================================================== #
 
 # Variables
-$path = "Your .csv file"
-$path_2 = "Your new .csv file"
+$ErrorActionPreference = "silentlycontinue"
+$path = Read-host "Entrez CORRECTEMENT le chemin du fichier csv a modifie"
 $users = Import-Csv -Path $path -Delimiter ";" -Header FirstName, Name
 
 # loop which modify firstname in CamelCase and Name in Upper
@@ -16,4 +17,15 @@ foreach($user in $users)
     $user.FirstName = $user.FirstName.replace($user.Firstname, (Get-Culture).TextInfo.ToTitleCase($user.FirstName))
     $user.Name = $user.Name.replace($user.Name, $user.Name.ToUpper())
 }
-$users | Export-Csv $path_2 -NoTypeInformation -Encoding UTF8 #modify file export
+$path_2 = Read-host "Entrez le chemin ou s'enregistrera le fichier csv modifie"
+
+# NULL case error gestion
+if ($error.Count -ieq 0){
+	Write-Host "Tout est Ok" -f Green
+}
+else{
+    Write-Host "Des erreurs d'execution liees a des cases vides dans le fichier ont eu lieu. Elles n'ont pas fraine la bonne execution du script" -f Yellow
+}
+
+# File export
+$users | Export-Csv $path_2 -NoTypeInformation -Encoding UTF8
